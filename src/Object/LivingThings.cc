ffe62b7ea0e21159e4894ca98c8f8ac455fa2b80
handsome0hell <handsome0hell@gmail.com>
2018-05-13 10:57:58 +0800

    Add copyright informations

--- src/Object/LivingThings.cc
+++ src/Object/LivingThings.cc
@@ -1,3 +1,19 @@
+//    Copyright (C) 2018  Handsome0hell
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//    Email: handsome0hell@gmail.com
 #include "LivingThings.h"
 #include "../CPP.h"
 inline uint32_t GetDifference(uint32_t a, uint32_t b) {
